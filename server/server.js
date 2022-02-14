35e21c3e3f7492a4af1b1d5b952bdd6a4be1d061
Nader Almogazy <nader73107@gmail.com>
2022-02-14 15:26:46 -0500

    updates to scripts and added cors requirement

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require("path");
 const app = express()
 const PORT = 3000
-
+const cors = require("cors")
 // Require Routers
 const api = require("./routes/api.js");
 
