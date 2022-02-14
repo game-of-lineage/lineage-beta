9dc8c77f80616bf8d57ca5da886278539aac743a
Nader Almogazy <nader73107@gmail.com>
2022-02-14 14:21:48 -0500

    Created server, routes, controllers, and placeholders for sign in and up

---
+++ server/controllers/boardController.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+const fetch = require("node-fetch");
+
+const boardController = {};
+
+boardController.saveBoard = (req, res, next) => {
+  
+}
+
+boardController.loadBoard = (req, res, next) => {
+  
+}
+
+boardController.postBoard = (req, res, next) => {
+  
+}
+
+boardController.loadBoardFromLexicon = (req, res, next) => {
+  
+}
+
+module.exports = boardController;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require("path");
+const app = express()
+const PORT = 3000
+
+// Require Routers
+const api = require("./routes/api.js");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', api)
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(PORT, ()=>console.log(`listening on port ${PORT}`))
+
+module.exports = app;
