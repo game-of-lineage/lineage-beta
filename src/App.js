3db90d3328d23c70335c89f09f1a4aa5219ee33c
Nader Almogazy <nader73107@gmail.com>
2022-02-16 08:52:29 -0500

    1 more optimization to the set

--- src/App.js
+++ src/App.js
@@ -14,53 +14,53 @@ const BOARD_HEIGHT = 40;
 
 const aliveCells = new Set();
 
-
 const App = ({ name }) => {
   // Version without simulation - commented out.
   //const [initialBoardState, setInitialBoardState] = useState((new Array(50).fill(new Array(100).fill(0)));
   //const [boardState, setBoardState] = useState((new Array(50).fill(new Array(100).fill(0)));
   const [initialBoardState, setInitialBoardState] = useState(
     simulateBoard(new Array(BOARD_HEIGHT).fill(new Array(BOARD_WIDTH).fill(0)))
-    );
-    const [boardState, setBoardState] = useState(
-      simulateBoard(new Array(BOARD_HEIGHT).fill(new Array(BOARD_WIDTH).fill(0)))
-      );
-      const [tick, setTick] = useState(true);
-      const [timer, setTimer] = useState(1000);
-      const [play, setPlay] = useState(false);
-      const [generation, setGeneration] = useState(0);
-      const [squareClicked, setSquareClicked] = useState(false)
-      
-      useEffect(() => {
-        if (play) {
-          const newBoardState = runGame(boardState, aliveCells);
-          setBoardState(newBoardState);
-          setTimeout(() => {
-            setGeneration(generation + 1);
-            setTick(!tick);
-          }, timer);
-        }
-      }, [tick, play]);
-      
-      useEffect(() => {
-        aliveCells.clear()
-        if (play) setInitialBoardState(boardState.map((row) => [...row]));
-      }, [play]);
+  );
+  const [boardState, setBoardState] = useState(
+    simulateBoard(new Array(BOARD_HEIGHT).fill(new Array(BOARD_WIDTH).fill(0)))
+  );
+  const [tick, setTick] = useState(true);
+  const [timer, setTimer] = useState(1000);
+  const [play, setPlay] = useState(false);
+  const [generation, setGeneration] = useState(0);
+  const [squareClicked, setSquareClicked] = useState(false);
 
-      const handleSquareClick = useCallback(
-        (row, col) => {
-          if(!play){
-            aliveCells.clear()
-            let newBoardState = boardState.map((row)=>[...row]);
-            newBoardState[row][col] = newBoardState[row][col] ? 0 : 1;
-            setSquareClicked(!squareClicked)
-            setBoardState(newBoardState);
-          }
-        }, [squareClicked, play]);
+  useEffect(() => {
+    if (play) {
+      const newBoardState = runGame(boardState, aliveCells);
+      setBoardState(newBoardState);
+      setTimeout(() => {
+        setGeneration(generation + 1);
+        setTick(!tick);
+      }, timer);
+    }
+  }, [tick, play]);
 
-      
-      return (
-        <>
+  useEffect(() => {
+    aliveCells.clear();
+    if (play) setInitialBoardState(boardState.map((row) => [...row]));
+  }, [play]);
+
+  const handleSquareClick = useCallback(
+    (row, col) => {
+      if (!play) {
+        aliveCells.clear();
+        let newBoardState = boardState.map((row) => [...row]);
+        newBoardState[row][col] = newBoardState[row][col] ? 0 : 1;
+        setSquareClicked(!squareClicked);
+        setBoardState(newBoardState);
+      }
+    },
+    [squareClicked, play]
+  );
+
+  return (
+    <>
       <div id="main">
         <ControlPanel
           initialBoardState={initialBoardState}
@@ -104,41 +104,33 @@ const runGame = (grid, aliveCells) => {
 
   // Update each Cell's nextGen value so that a new grid can be printed.
   //If we noted past alive cells in the Set, only check around alive cells.
-  if(aliveCells.size){
+  if (aliveCells.size) {
     // instantiate Set iterator FROM CLONED SET
-    const iterator = new Set(aliveCells).values()
+    const iterator = new Set(aliveCells).values();
     // set current to iterator.next() *the first element in the set*
-    let current = iterator.next()
+    let current = iterator.next();
     // Clear original set, that's why we cloned.
-    aliveCells.clear()
+    aliveCells.clear();
     //iterate through set
-    while(!current.done){
+    while (!current.done) {
       // turn stringified coordinate back into numbers
-      const strings  = current.value.split(",")
-      const row = Number(strings[0])
-      const col = Number(strings[1])
+      const strings = current.value.split(",");
+      const row = Number(strings[0]);
+      const col = Number(strings[1]);
 
       // update Cell and neighbors.
       updateCell(row, col);
-      if (col > 0) updateCell(row, col-1)
-      if (row > 0) updateCell(row-1, col)
-      if (col < COLS - 1) updateCell(row, col+1)
-      if (row < ROWS - 1) updateCell(row+1, col)
-      if (col < COLS - 1 && row > 0) updateCell(row-1, col+1)
-      if (row < ROWS - 1 && col > 0) updateCell(row+1, col-1)
-      if (row < ROWS - 1 && col < COLS - 1) updateCell(row+1, col+1)
-      if (row > 0 && col > 0) updateCell(row-1, col-1);
-      
+
       //increment current
-      current = iterator.next()
+      current = iterator.next();
     }
     //if no set, loop through entire board.
   } else {
-      for (let row = 0; row < ROWS; row++) {
-        for (let col = 0; col < COLS; col++) {
-          updateCell(row, col);
-        }
+    for (let row = 0; row < ROWS; row++) {
+      for (let col = 0; col < COLS; col++) {
+        updateCell(row, col);
       }
+    }
   }
 
   // Function calculates number of neighbors (8) that are 1s. Based on number of neighbors,
@@ -161,22 +153,39 @@ const runGame = (grid, aliveCells) => {
 
     if (row > 0 && col > 0) neighborsAlive += grid[row - 1][col - 1];
 
+    const addNeighbors = (row, col) => {
+      aliveCells.add(row + "," + col);
+      if (row < ROWS - 1 && col < COLS - 1) aliveCells.add(`${row+1},${col+1}`);
+
+      if (col < COLS - 1) aliveCells.add(`${row},${col+1}`);
+      if (row < ROWS - 1) aliveCells.add(`${row+1},${col}`);
+
+      if (col < COLS - 1 && row > 0) aliveCells.add(`${row-1},${col+1}`);
+      if (row < ROWS - 1 && col > 0) aliveCells.add(`${row+1},${col-1}`);
+
+      if (col > 0) aliveCells.add(`${row},${col-1}`);
+      if (row > 0) aliveCells.add(`${row-1},${col}`);
+
+      if (row > 0 && col > 0) aliveCells.add(`${row-1},${col-1}`);
+    };
+
     // If dead
     if (!alive) {
       if (neighborsAlive === 3) {
         newBoard[row][col] = 1;
-        aliveCells.add(row + "," + col);
+        addNeighbors(row, col);
       }
     }
     // If alive
     if (alive) {
       if (neighborsAlive === 2 || neighborsAlive === 3) {
         newBoard[row][col] = 1;
-        aliveCells.add(row + "," + col);
+        addNeighbors(row, col);
       } else {
         newBoard[row][col] = 0;
       }
     }
+    
   }
   return newBoard;
 };
